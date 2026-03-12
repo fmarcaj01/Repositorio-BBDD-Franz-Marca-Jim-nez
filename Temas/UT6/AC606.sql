c8e062967a9e03b55ef629499de985e4a382bb72
Hola Mundo <franzmarcajimenez@gmail.com>
2026-03-12 20:31:55 +0100

    AC606

--- Temas/UT6/AC606.sql
+++ Temas/UT6/AC606.sql
@@ -1,17 +1,17 @@
 -- 1
-UPDATE productos p
-JOIN proveedores prov ON p.proveedor_id = prov.id
-SET p.precio = p.precio * 0.90
-WHERE prov.ciudad = 'Madrid';
+UPDATE productos e
+JOIN proveedores p ON e.proveedor_id = p.id
+SET e.precio = Round(e.precio - (e.precio * 0.10),2)
+WHERE p.ciudad = 'Madrid';
 
 -- 2
-UPDATE productos p
-JOIN proveedores prov ON p.proveedor_id = prov.id
-SET p.activo = FALSE
-WHERE prov.activo = FALSE;
+UPDATE productos e
+JOIN proveedores p ON p.proveedor_id = p.id
+SET e.activo = FALSE
+WHERE p.nombre = 'GlobalTech SA';
 
 -- 3 
-UPDATE productos p
-JOIN proveedores prov ON p.proveedor_id = prov.id
-SET p.activo = FALSE
-WHERE prov.activo = FALSE;
+delete p
+from productos e
+join proveedores p on e.proveedor_id = p.id
+where p.ciudad = 'Barcelonaa' and e.stock = 0 
